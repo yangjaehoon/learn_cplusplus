16bffb5a04a575bae9f02267d5fc642990bd6b2b
yangjaehoon <lljhms@naver.com>
2022-01-04 17:11:37 +0900

    20220104

---
+++ HelloWrold.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main()
+{
+    int num=20;
+
+    return 0;
+}
---
+++ SimpleAdder.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main(void)
+{
+    int val1;
+    std::cout<<"첫 번째 숫자입력: ";
+    std::cin>>val2;
+}
