060bd972dff53a1c4090d4624e6b74c7c1d52704
yangjaehoon <lljhms@naver.com>
2022-01-04 17:29:36 +0900

    20220104

--- HelloWrold.cpp
+++ HelloWrold.cpp
@@ -3,6 +3,10 @@
 int main()
 {
     int num=20;
+    std::cout <<"Hello World!"<<std::endl;
+    std::cout <<"Hello "<<"World!"<<std::endl;
+    std::cout <<num<<' '<<'A';
+    std::cout << ' '<<3.14<<std::endl;
 
     return 0;
 }
