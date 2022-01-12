ca55e8c0cbc2d075d1923d252ba27a02a235986d
yangjaehoon <lljhms@naver.com>
2022-01-13 00:41:44 +0900

    20220113

---
+++ 132p_CarInline/RacingInlineMain.cpp
@@ -0,0 +1,18 @@
+#include "CarInline.h"
+
+int main()
+{
+    Car run99;
+    run99.InitMembers("run99", 100);
+    run99.Accel();
+    run99.Accel();
+    run99.Accel();
+    run99.ShowCarState();
+    run99.Break();
+    run99.ShowCarState();
+    return 0;
+}
+
+//컴파일 하는방법
+//g++ Car.cpp RacingMain.cpp -o 이름
+// ./이름
